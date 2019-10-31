84e066aa75d0a9282d25401da27053c917b9ee00
vicabitu <victor.ch370@gmail.com>
2019-10-31 14:34:24 -0300

    Definiendo los modelos

--- administracion/models.py
+++ administracion/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    es_medico = models.BooleanField(default=False)
+    es_paciente = models.BooleanField(default=False)
+
+class Medico(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, blank=True, null=True)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    especialidad = models.CharField(max_length=30)
+
+class Paciente(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, blank=True, null=True)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=30)
+    fecha_de_nacimiento = models.DateField()
--- clinica/settings.py
+++ clinica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administracion',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+AUTH_USER_MODEL='administracion.Usuario'
 
 STATIC_URL = '/static/'
 
