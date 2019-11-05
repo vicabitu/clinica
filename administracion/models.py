ee43af6371eb6a6edc9004a8b1b6d918db6fd510
vicabitu <victor.ch370@gmail.com>
2019-11-05 19:48:40 -0300

    Borrando campo y str

--- administracion/models.py
+++ administracion/models.py
@@ -5,13 +5,32 @@ class Usuario(AbstractUser):
     es_medico = models.BooleanField(default=False)
     es_paciente = models.BooleanField(default=False)
 
+    def __str__(self):
+        if self.es_medico:
+            cadena = "Medico"
+        else:
+            cadena = "Paciente"
+        return "{} - {} - {}".format(self.first_name, self.last_name, cadena)
+
+    def get_view_name(self):
+        if self.es_medico:
+            return 'medico'
+        else:
+            return 'paciente'
+
+
 class Medico(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, blank=True, null=True)
-    apellido = models.CharField(max_length=30)
     especialidad = models.CharField(max_length=30)
+
+    def __str__(self):
+        return  "{} - {} - {}".format(self.usuario.first_name, self.usuario.last_name, self.especialidad)
 
 class Paciente(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, blank=True, null=True)
     direccion = models.CharField(max_length=30)
     telefono = models.CharField(max_length=30)
     fecha_de_nacimiento = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.usuario.first_name, self.usuario.last_name)
