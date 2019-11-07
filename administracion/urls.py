895975892ad732a810e9adcd10bd33eb427e9b9c
vicabitu <victor.ch370@gmail.com>
2019-11-07 11:29:32 -0300

    Listado de pacientes y de historias medicas

--- administracion/urls.py
+++ administracion/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('medico', views.mostrar_index_medico, name='medico'),
-    path('paciente', views.mostrar_index_paciente, name='paciente')
+    path('paciente', views.mostrar_index_paciente, name='paciente'),
+    path('historias_medicas_medico', views.ListarHistoriasClinicasMedico.as_view(), name='historias_medicas_medico'),
+    path('pacientes', views.ListarPacientes.as_view(), name='pacientes'),
+    path('detalle_historia_medica/<int:pk>', views.DetalleHistoriaMedica.as_view(), name='detalle_historia_medica')
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import View, CreateView, ListView, DetailView, TemplateView, FormView
+from .models import HistoriaMedica, Paciente
 
 def mostrar_index_medico(request):
     return render(request, 'medico/medico.html')
 
 def mostrar_index_paciente(request):
     return render(request, 'paciente/paciente.html')
+
+class ListarHistoriasClinicasMedico(ListView):
+    model = HistoriaMedica
+    template_name = 'medico/historias_medicas.html'
+    context_object_name = 'historias_medicas'
+
+    def get_queryset(self):
+        return HistoriaMedica.objects.filter(medico=self.request.user.medico)
+
+class ListarPacientes(ListView):
+    model = Paciente
+    template_name = 'medico/pacientes.html'
+    context_object_name = 'pacientes'
+
+    def get_queryset(self):
+        return Paciente.objects.filter(historias_medicas__medico=self.request.user.medico)
+
+class DetalleHistoriaMedica(DetailView):
+    model = HistoriaMedica
+    template_name = 'medico/detalle_historia_medica.html'
+    context_object_name = 'historia'
